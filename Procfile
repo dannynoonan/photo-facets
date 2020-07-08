a25975a765d80ab3d84094ba38cb0b9522d5bcab
Andy Shirey <andyshirey@ilovelamp.local>
2020-07-08 13:19:20 -0700

    heroku deployment troubleshooting cont'd

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py runserver
+web: gunicorn photo_facets.wsgi --log-file -
--- photo_facets/wsgi.py
+++ photo_facets/wsgi.py
@@ -10,7 +10,10 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'photo_facets.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+# https://stackoverflow.com/questions/22007428/error-r10-boot-timeout-django-app-on-heroku
+application = Cling(get_wsgi_application())
