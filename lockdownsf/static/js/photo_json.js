cc4c25006aec2765c89cb6a14fd48aa25957a601
Andy Shirey <andyshirey@ilovelamp.local>
2020-07-08 00:26:28 -0700

    port photo-map visualization prototype into django project, initialize db model, set up templates, configure placeholder views

---
+++ lockdownsf/static/js/photo_json.js
@@ -0,0 +1,20 @@
+photoCollection = [
+    {
+        neighborhood: 'hayesValley',
+        photos: [
+            {id: 1404, format: 'landscape', cats: ['park'], isPark: true},
+            {id: 2747, format: 'pano', cats: ['mural'], isMural: true},
+            {id: 2760, format: 'landscape', cats: ['mural', 'dining'], isMural: true, isDining: true},
+            {id: 4435, format: 'landscape', cats: ['mural', 'exercise'], isMural: true, isExercise: true},
+        ]
+    },
+    {
+        neighborhood: 'missionValencia',
+        photos: [
+            {id: 2451, format: 'landscape', cats: ['mural', 'financial'], isMural: true, isFinancial: true},
+            {id: 3912, format: 'landscape', cats: ['mural', 'nonFoodShop'], isMural: true, isNonFoodShop: true},
+            {id: 4549, format: 'landscape', cats: ['mural', 'blm'], isMural: true, isBLM: true},
+            {id: 5962, format: 'landscape', cats: ['park'], isPark: true},
+        ]
+    }, 
+];
