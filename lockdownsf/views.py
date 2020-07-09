6fc7e741f3af0392cca44775aba051071f4fbdb9
Andy Shirey <andyshirey@ilovelamp.local>
2020-07-09 00:15:03 -0700

    transition to postgresql (only for local dev so far)

--- lockdownsf/views.py
+++ lockdownsf/views.py
@@ -7,15 +7,18 @@ from .models import Neighborhood
 
 
 def index(request):
-    # neighborhoods = Neighborhood.objects.all()
-    neighborhoods = []
+    neighborhoods = Neighborhood.objects.all()
     context = {'neighborhoods': neighborhoods}
     return render(request, 'index.html', context)
 
 def neighborhood(request, neighborhood_slug):
     try:
         neighborhood = Neighborhood.objects.get(pk=neighborhood_slug)
-        context = {'neighborhood': neighborhood}
+        photos = neighborhood.photo_set.all() 
+        context = {
+            'neighborhood': neighborhood,
+            'photos': photos,
+        }
         return render(request, 'neighborhood.html', context)
     except Neighborhood.DoesNotExist:
         raise Http404("No neighborhood found for slug %s" % neighborhood_slug)
