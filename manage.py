13a573839e836e2ff178ff25c74a13d9e21349af
Andy Shirey <andyshirey@ilovelamp.local>
2020-07-12 07:35:32 -0700

    s3 authentication and img file uploading

--- manage.py
+++ manage.py
@@ -5,14 +5,14 @@ import sys
 
 
 def main():
+    # override with: python manage.py runserver --settings=<another_settings_module>
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django. Make sure it's installed and available on your PYTHONPATH" 
+            "environment variable, and that you've activated a virtual environment."
         ) from exc
     execute_from_command_line(sys.argv)
 
