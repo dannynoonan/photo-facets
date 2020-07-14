756609892931d06724551034acd872e7e02f01af
Andy Shirey <andyshirey@ilovelamp.local>
2020-07-14 16:33:36 -0700

    photo metadata form submission and Photo object data binding

--- lockdownsf/urls.py
+++ lockdownsf/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('neighborhood/<str:neighborhood_slug>/', views.neighborhood, name='neighborhood'),
     path('sign_s3/', views.sign_s3, name='sign_s3'),
-    path('upload_photo/', views.upload_photo, name='upload_photo'),
-    path('submit_form/', views.submit_form, name='submit_form'),
+    path('select_photo/', views.select_photo, name='select_photo'),
+    path('save_photo/', views.save_photo, name='save_photo'),
 ]
