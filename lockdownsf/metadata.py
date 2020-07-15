4d1c7bac7497b45a5e0c291d6f09d07664939010
Andy Shirey <andyshirey@ilovelamp.local>
2020-07-15 00:24:28 -0700

    photo model writes and schema reboot

---
+++ lockdownsf/metadata.py
@@ -0,0 +1,48 @@
+
+
+all_aspect_formats = [
+    'landscape',
+    'portrait',
+    'square',
+    'pano',
+    'pano_vertical',
+]
+
+all_scene_types = [
+    'boarded',
+    'boarded_tagged',
+    'boarded_mural',
+    'sign_generic',
+    'sign_distinctive',
+    'sign_personal',
+    'park',
+    'slow_streets',
+    'empty_street',
+    'none',
+]
+
+all_business_types = [
+    'food_market',
+    'non_food_shop',
+    'dining',
+    'bar',
+    'laundry',
+    'salon',
+    'exercise',
+    'medical',
+    'financial',
+    'performance_venue',
+    'municipal',
+    'religious', 
+]
+
+all_other_labels = [
+    'open',
+    'clever',
+    'neighborly',
+    'black_lives_matter',
+]
+
+image_file_formats = {
+    'image/jpeg': 'jpg'
+}
