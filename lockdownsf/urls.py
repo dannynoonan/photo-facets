c59fc06e1e99e2b474751001487ccba016fc832e
Andy Shirey <andyshirey@ilovelamp.local>
2020-07-15 17:51:15 -0700

    neighborhood listing/add/edit workflow, change fk relation to photos to be id-based

--- lockdownsf/urls.py
+++ lockdownsf/urls.py
@@ -6,6 +6,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('neighborhood/<str:neighborhood_slug>/', views.neighborhood, name='neighborhood'),
     path('sign_s3/', views.sign_s3, name='sign_s3'),
-    path('select_photo/', views.select_photo, name='select_photo'),
-    path('save_photo/', views.save_photo, name='save_photo'),
+    path('admin/neighborhood_listing/', views.neighborhood_listing, name='neighborhood_listing'),
+    path('admin/add_neighborhood/', views.add_neighborhood, name='add_neighborhood'),
+    path('admin/save_neighborhood/', views.save_neighborhood, name='save_neighborhood'),
+    path('admin/edit_neighborhood/<str:neighborhood_slug>/', views.edit_neighborhood, name='edit_neighborhood'),
+    path('admin/select_photo/', views.select_photo, name='select_photo'),
+    path('admin/save_photo/', views.save_photo, name='save_photo'),
+    path('admin/edit_photo/<str:photo_uuid>/', views.edit_photo, name='edit_photo'),
 ]
