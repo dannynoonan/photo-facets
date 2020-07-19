9a1d70f794ac78aaa9a2f8a89d5bb9eecbd3a21d
Andy Shirey <andyshirey@ilovelamp.local>
2020-07-19 00:39:14 -0700

    turns out heroku's came with the frame postres sich is some bullshit

--- config/settings/heroku.py
+++ config/settings/heroku.py
@@ -7,11 +7,12 @@ import psycopg2
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 
 # Postgresql 
 # https://devcenter.heroku.com/articles/heroku-postgresql
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+HEROKU_POSTGRESQL_BRONZE_URL = os.environ['HEROKU_POSTGRESQL_BRONZE_URL']
+conn = psycopg2.connect(HEROKU_POSTGRESQL_BRONZE_URL, sslmode='require')
 
