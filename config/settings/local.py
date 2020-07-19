3e8b61a599f6ce70540bce8ef359a5bd23b6a89b
Andy Shirey <andyshirey@ilovelamp.local>
2020-07-18 22:46:43 -0700

    cleanly split declarations of DATABASE param between local and heroku configs

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,1 +1,12 @@
 from .base import *
+
+
+# Database
+# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lockdownsf',
+    }
+}
