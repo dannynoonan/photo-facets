cb3b191293e3bbdeed6bf5b379e9a95bc16d2d79
Andy Shirey <andyshirey@ilovelamp.local>
2020-07-19 20:26:04 -0700

    for images having > 50 words returned by rekognition detect_text, make second OCR call using textract detect_document_text

--- lockdownsf/service.py
+++ lockdownsf/service.py
@@ -54,23 +54,40 @@ def detect_text(photo, bucket):
     # https://docs.aws.amazon.com/rekognition/latest/dg/text-detecting-text-procedure.html
     # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/rekognition.html#Rekognition.Client.detect_text
 
-    # TODO could this extract more than 50 words?
-    # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/textract.html
+    # TODO automatically process personalized signs with textract, for everything else start with 
 
     client = boto3.client('rekognition', region_name=AWS_REGION_NAME)
-    response = client.detect_text(Image={'S3Object':{'Bucket':bucket,'Name':photo}})
+    response = client.detect_text(Image={'S3Object': {'Bucket': bucket, 'Name': photo}})
                         
     text_detections = response['TextDetections']
     extracted_text_raw = ''
     extracted_text_formatted = ''
-    for text in text_detections:
-        if text['Type'] == 'LINE':
-            extracted_text_raw = f"{extracted_text_raw} {text['DetectedText']}"
-            extracted_text_formatted = f"{extracted_text_formatted}<br/>{text['DetectedText']}"
-    
-    print ('extracted_text_raw')
-    print (extracted_text_raw)
-    print ('text_format')
-    print (extracted_text_formatted)
+
+    word_count = len([t for t in text_detections if t['Type'] == 'WORD'])
+    print(f"word count: {word_count}")
+
+    # rekognition detect_text maxes out at 50 words, if fewer than 50 words then parse response  
+    if word_count < 50:
+        print('parsing rekognition detect_text response')
+        for text in text_detections:
+            if text['Type'] == 'LINE':
+                extracted_text_raw = f"{extracted_text_raw} {text['DetectedText']}"
+                extracted_text_formatted = f"{extracted_text_formatted}<br/>{text['DetectedText']}"
+ 
+    # otherwise use textract's detect_document_text
+    else:
+        print('parsing textract detect_document_text response')
+        client = boto3.client('textract', region_name=AWS_REGION_NAME)
+        response = client.detect_document_text(Document={'S3Object': {'Bucket': bucket, 'Name': photo}})
+        blocks = response['Blocks']
+        for block in blocks:
+            if 'Text' in block and block['BlockType'] == 'LINE':
+                extracted_text_raw = f"{extracted_text_raw} {block['Text']}"
+                extracted_text_formatted = f"{extracted_text_formatted}<br/>{block['Text']}"
+
+    print('extracted_text_raw')
+    print(extracted_text_raw)
+    print('extracted_text_formatted')
+    print(extracted_text_formatted)
 
     return extracted_text_raw, extracted_text_formatted
