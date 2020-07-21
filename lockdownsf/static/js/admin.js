a5916539933b09ebb968886be5763dc682971cf3
Andy Shirey <andyshirey@ilovelamp.local>
2020-07-21 00:26:51 -0700

    refactor home page to source photos from db and dynamically generate facet checkboxes

--- lockdownsf/static/js/admin.js
+++ lockdownsf/static/js/admin.js
@@ -159,3 +159,12 @@ function extractImageData() {
         document.getElementById("photo-date-taken").value = dateTaken;
     });
 }
+
+
+function convertDMSToDD(degrees, minutes, seconds, direction) { 
+    var dd = degrees + (minutes/60) + (seconds/3600);
+    if (direction == "S" || direction == "W") {
+        dd = dd * -1; 
+    }
+    return dd;
+}
