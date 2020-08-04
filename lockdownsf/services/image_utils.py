4609538e4a1cc163ea821948c0923a9668a77851
Andy Shirey <andyshirey@ilovelamp.local>
2020-08-04 13:23:42 -0700

    refactor api and util modules into service package

---
+++ lockdownsf/services/image_utils.py
@@ -0,0 +1,47 @@
+from lockdownsf.metadata import img_max_dimensions
+
+
+def calculate_resized_images(aspect_ratio, width, height):
+    img_dimensions = {}
+    # landscape, square, or pano
+    if aspect_ratio >= 1:
+        # small
+        small_width = img_max_dimensions['small_width']
+        small_height = round(img_max_dimensions['small_width'] / aspect_ratio)
+        img_dimensions['small'] = (small_width, small_height)
+        # medium
+        if img_max_dimensions['medium_width'] > width:
+            img_dimensions['medium'] = (width, height)
+        else:
+            medium_width = img_max_dimensions['medium_width']
+            medium_height = round(img_max_dimensions['medium_width'] / aspect_ratio)
+            img_dimensions['medium'] = (medium_width, medium_height)
+        # large
+        if img_max_dimensions['large_width'] > width:
+            img_dimensions['large'] = (width, height)
+        else:
+            large_width = img_max_dimensions['large_width']
+            large_height = round(img_max_dimensions['large_width'] / aspect_ratio)
+            img_dimensions['large'] = (large_width, large_height)
+    # portrait or vertical pano
+    else:
+        # small
+        small_height = img_max_dimensions['small_height']
+        small_width = round(img_max_dimensions['small_height'] * aspect_ratio)
+        img_dimensions['small'] = (small_width, small_height)
+        # medium
+        if img_max_dimensions['medium_height'] > height:
+            img_dimensions['medium'] = (width, height)
+        else:
+            medium_height = img_max_dimensions['medium_height']
+            medium_width = round(img_max_dimensions['medium_height'] * aspect_ratio)
+            img_dimensions['medium'] = (medium_width, medium_height)
+        # large
+        if img_max_dimensions['large_height'] > height:
+            img_dimensions['large'] = (width, height)
+        else:
+            large_height = img_max_dimensions['large_height']
+            large_width = round(img_max_dimensions['large_height'] * aspect_ratio)
+            img_dimensions['large'] = (large_width, large_height)
+            
+    return img_dimensions
