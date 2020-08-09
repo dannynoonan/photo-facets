2dcd158ed4846752ed386a16d92724d9a962235c
Andy Shirey <andyshirey@ilovelamp.local>
2020-08-08 21:32:29 -0700

    refactor models to add Tag and User tables and relations, rename 'admin' pages to 'manage' to avoid namespace conflict with django 'admin' defaults

--- lockdownsf/models.py
+++ lockdownsf/models.py
@@ -35,11 +35,22 @@ class Photo(models.Model):
         return self.uuid + ' (' + self.neighborhood.slug + ' / ' + self.source_file_name + ')'
 
 
+class User(models.Model):
+    email = models.CharField(max_length=512, db_index=True, unique=True)
+    dt_inserted = models.DateTimeField(auto_now_add=True)
+    dt_last_login = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=64, db_index=True)
+
+    def __str__(self):
+        return self.email
+
+
 class Album(models.Model):
     external_id = models.CharField(max_length=500, db_index=True, null=True, unique=True)
-    external_resource = models.CharField(max_length=64, db_index=True, null=True)
-    name = models.CharField(max_length=500)
-    description = models.CharField(max_length=500, null=True)
+    external_resource = models.CharField(max_length=64, db_index=True, null=True)  # chopping block
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=500)  # chopping block
+    description = models.CharField(max_length=500, null=True)  # chopping block
     center_latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
     center_longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
     map_zoom_level = models.IntegerField(default=0)
@@ -48,28 +59,43 @@ class Album(models.Model):
     status = models.CharField(max_length=64, db_index=True)
 
     def __str__(self):
-        return f"{self.name}|{self.external_resource}|{self.external_id}|{self.status}"
+        return f"{self.name}|{self.external_id}"
 
 
 class MediaItem(models.Model):
     external_id = models.CharField(max_length=500, db_index=True, null=True, unique=True)
-    external_resource = models.CharField(max_length=64, db_index=True, null=True)
+    external_resource = models.CharField(max_length=64, db_index=True, null=True)  # chopping block
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
     file_name = models.CharField(max_length=256, db_index=True)
-    mime_type = models.CharField(max_length=128, db_index=True)
-    thumb_url = models.CharField(max_length=2048, null=True)
-    description = models.CharField(max_length=500, null=True)
-    dt_taken = models.DateTimeField(null=True)
+    mime_type = models.CharField(max_length=128, db_index=True)  # chopping block
+    thumb_url = models.CharField(max_length=2048, null=True)  # chopping block
+    description = models.CharField(max_length=500, null=True)  # chopping block
+    dt_taken = models.DateTimeField(null=True)  # chopping block
     dt_inserted = models.DateTimeField(auto_now_add=True)
     dt_updated = models.DateTimeField(auto_now=True)
-    width = models.IntegerField(default=0)
-    height = models.IntegerField(default=0)
+    width = models.IntegerField(default=0)  # chopping block
+    height = models.IntegerField(default=0)  # chopping block
     latitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, null=True)
-    facets = models.CharField(max_length=1024, null=True)
+    facets = models.CharField(max_length=1024, null=True)  # chopping block
     extracted_text = models.CharField(max_length=16000, null=True)
     status = models.CharField(max_length=64, db_index=True)
 
     def __str__(self):
-        return f"{self.file_name}|{self.mime_type}|{self.external_resource}|{self.external_id}|{self.status}"
+        return f"{self.file_name}|{self.external_id}"
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, db_index=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    dt_inserted = models.DateTimeField(auto_now_add=True)
+    media_items = models.ManyToManyField(MediaItem)
+    status = models.CharField(max_length=64, db_index=True)
+
+    class Meta:
+        unique_together = ('name', 'owner')
+
+    def __str__(self):
+        return self.name
     
