1bc26d14fe72f9b798e09429954da623052f1e4a
Andy Shirey <andyshirey@ilovelamp.local>
2020-09-02 15:38:59 -0700

    add google site verification page

--- config/urls.py
+++ config/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+
+from lockdownsf.views import google_site_verification
+
 
 urlpatterns = [
     path('lockdownsf/', include('lockdownsf.urls')),
     path('admin/', admin.site.urls),
+    path(settings.GOOGLE_SITE_VERIFICATION, google_site_verification),
 ]
