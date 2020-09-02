85ada7075b1422c05edd37752c3ef0b0cb6e19cf
Andy Shirey <andyshirey@ilovelamp.local>
2020-09-02 00:31:33 -0700

    figure out whether google photos api was ever working from heroku app

--- lockdownsf/services/gphotosapi.py
+++ lockdownsf/services/gphotosapi.py
@@ -20,8 +20,8 @@ from django.conf import settings
 from lockdownsf.services import image_utils
 
 
-# CREDENTIALS_FILE = join(dirname(__file__), 'gphotos_credentials.json')
-CREDENTIALS_FILE = '/tmp/gphotos_credentials.json'
+CREDENTIALS_FILE = join(dirname(__file__), 'gphotos_credentials.json')
+# CREDENTIALS_FILE = '/tmp/gphotos_credentials.json'
 GPHOTOS_UPLOAD_URL = 'https://photoslibrary.googleapis.com/v1/uploads'
 TOKEN_PICKLE_FILE = 'token.pickle'
 # SCOPES = [
@@ -36,8 +36,8 @@ CLIENT_CONFIG = {
     'web': {
         'client_id': settings.GOOGLE_CLIENT_ID,
         'client_secret': settings.GOOGLE_CLIENT_SECRET,
-        'redirect_uris': [settings.GOOGLE_REDIRECT_URI],
-        #'redirect_uris': ["http://localhost:8080/auth/google/callback", "https://arcane-island-30983.herokuapp.com/auth/google/callback"],
+        # 'redirect_uris': [settings.GOOGLE_REDIRECT_URI],
+        'redirect_uris': ["http://localhost:8080/auth/google/callback", "https://arcane-island-30983.herokuapp.com/auth/google/callback"],
         'auth_uri': 'https://accounts.google.com/o/oauth2/auth',
         'token_uri': 'https://accounts.google.com/o/oauth2/token',
 
@@ -49,6 +49,25 @@ CLIENT_CONFIG = {
 
 
 def init_gphotos_service():
+    creds = None
+    if exists(TOKEN_PICKLE_FILE):
+        with open(TOKEN_PICKLE_FILE, 'rb') as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(CREDENTIALS_FILE, SCOPES)
+            creds = flow.run_local_server()
+        with open(TOKEN_PICKLE_FILE, 'wb') as token:
+            pickle.dump(creds, token)
+
+    gphotos_service = build(API_NAME, API_VERSION, credentials=creds)
+
+    return gphotos_service
+
+
+def init_gphotos_service_new():
     creds = None
     if exists(TOKEN_PICKLE_FILE):
         with open(TOKEN_PICKLE_FILE, 'rb') as token:
