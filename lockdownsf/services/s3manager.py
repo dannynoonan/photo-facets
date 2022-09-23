e315bdead9654f4d2e443abf7f2745a8a5c41d0f
Andy Shirey <andyshirey@ilovelamp.local>
2022-09-22 23:49:08 -0700

    remove gphotos integrations, replace with s3 dir mappings to albums

--- lockdownsf/services/s3manager.py
+++ lockdownsf/services/s3manager.py
@@ -70,95 +70,181 @@ def extract_text(img_file_name, bucket):
     return extracted_text_search, extracted_text_display
 
 
-def upload_image_to_s3(img_src_file_path, img_dest_file_name):
-    response = requests.get(img_src_file_path, stream=True)
-    orig_img = Image.open(BytesIO(response.content))
+# def upload_image_to_s3(img_src_file_path, img_dest_file_name):
+#     response = requests.get(img_src_file_path, stream=True)
+#     orig_img = Image.open(BytesIO(response.content))
 
-    in_mem_file = BytesIO()
-    orig_img.save(in_mem_file, format=orig_img.format)
-    in_mem_file.seek(0)
-
-    # Upload image to s3
-    client_s3 = boto3.client('s3',
-         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-
-    response = client_s3.put_object( 
-        ACL="public-read",
-        Bucket=settings.S3_BUCKET,
-        Body=in_mem_file,
-        ContentType=orig_img.format,
-        Key=img_dest_file_name,
-        Expires = datetime.now() + timedelta(minutes = 6),
-    )
-
-    img_dest_file_path = f"https://{settings.S3_BUCKET}.s3.amazonaws.com/{img_dest_file_name}"
-
-    return img_dest_file_path
-
-
-def delete_dir(tmp_dir, file_names):
-
-    # import ipdb; ipdb.set_trace()
-
-    if not (tmp_dir and file_names):
-        return
-    s3_resource = boto3.resource('s3',
-         aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-         aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-         
-    # delete each file in tmp_dir
-    for fn in file_names:
-        file_path = f"{tmp_dir}/{fn}"
-        try:
-            s3_resource.Object(settings.S3_BUCKET, file_path).delete()
-        except Exception as ex:
-            raise Exception(f"Failed to delete file [{file_path}] from s3. Details: {ex}")
-
-    # delete tmp_dir
-    try:
-        s3_resource.Object(settings.S3_BUCKET, tmp_dir).delete()
-    except Exception as ex:
-        raise Exception(f"Failed to delete temp directory [{tmp_dir}] from s3. Details: {ex}")
-
-
-# def resize_and_upload(orig_img, thumb_type, img_dimensions, uuid):
-#     print('****** in resize_and_upload')
-
-#     print(f"@@@@@@ [{thumb_type} before resizing] orig_img.format: {str(orig_img.format)}")
-#     print(f"@@@@@@ [{thumb_type} before resizing] orig_img.size: {str(orig_img.size)}")
-
-#     # im = Image.open(orig_img)
-#     orig_img.thumbnail(img_dimensions, Image.ANTIALIAS)
-    
-#     print(f"****** [{thumb_type} after resizing] orig_img.format: {str(orig_img.format)}")
-#     print(f"****** [{thumb_type} after resizing] orig_img.size: {str(orig_img.size)}")
-    
 #     in_mem_file = BytesIO()
 #     orig_img.save(in_mem_file, format=orig_img.format)
-
-#     print(f"^^^^^^ [{thumb_type}] orig_img saved to in_mem_file")
-#     print(f"^^^^^^ [{thumb_type}] file size / in_mem_file.tell(): {str(in_mem_file.tell())}")
-
 #     in_mem_file.seek(0)
 
-#     resized_img_file_name = f"{thumb_type}/{uuid}"
-
 #     # Upload image to s3
-#     client_s3 = boto3.client('s3') 
+#     client_s3 = boto3.client('s3',
+#          aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+#          aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
 
 #     response = client_s3.put_object( 
 #         ACL="public-read",
 #         Bucket=settings.S3_BUCKET,
 #         Body=in_mem_file,
-#         ContentType='image/jpeg',
-#         Key=resized_img_file_name,
+#         ContentType=orig_img.format,
+#         Key=img_dest_file_name,
 #         Expires = datetime.now() + timedelta(minutes = 6),
 #     )
 
-#     resized_img_file_path = f"https://{settings.S3_BUCKET}.s3.amazonaws.com/{resized_img_file_name}"
+#     img_dest_file_path = f"https://{settings.S3_BUCKET}.s3.amazonaws.com/{img_dest_file_name}"
 
-#     print(f"====== [{thumb_type}] resized_img_file_path: {resized_img_file_path}")
-#     print(f"====== [{thumb_type}] str(response): {str(response)}")
+#     return img_dest_file_path
 
-#     return resized_img_file_path
+
+# def delete_dir_old(dir_to_delete, file_names):
+#     if not (dir_to_delete and file_names):
+#         return
+#     s3_resource = boto3.resource('s3',
+#          aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+#          aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+         
+#     # delete each file in dir_to_delete
+#     for fn in file_names:
+#         file_path = f"{dir_to_delete}/{fn}"
+#         try:
+#             s3_resource.Object(settings.S3_BUCKET, file_path).delete()
+#         except Exception as ex:
+#             raise Exception(f"Failed to delete file [{file_path}] from s3. Details: {ex}")
+
+#     # delete dir_to_delete
+#     try:
+#         s3_resource.Object(settings.S3_BUCKET, dir_to_delete).delete()
+#     except Exception as ex:
+#         raise Exception(f"Failed to delete temp directory [{dir_to_delete}] from s3. Details: {ex}")
+
+
+def delete_dir(dir_to_delete: str) -> None:
+    s3_client = boto3.client('s3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+
+    response = s3_client.list_objects_v2(
+        Bucket=settings.S3_BUCKET,
+        Prefix=f"{dir_to_delete}/")
+
+    for content in response.get('Contents', []):
+        delete_file(content['Key'])
+
+    # delete_empty_dir(dir_to_delete)
+
+
+# def delete_empty_dir(dir_to_delete: str) -> None:
+#     s3_resource = boto3.resource('s3',
+#          aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+#          aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+
+#     try:
+#         print(f"Attempting to delete dir [{dir_to_delete}]")
+#         s3_resource.Object(settings.S3_BUCKET, dir_to_delete).delete()
+#         print(f"Successfully deleted s3 dir [{dir_to_delete}] from bucket [{settings.S3_BUCKET}]")
+#     except Exception as ex:
+#         raise Exception(f"Failed to delete s3 dir [{dir_to_delete}] from bucket [{settings.S3_BUCKET}]. Details: {ex}")
+
+
+def delete_file(path_to_file: str) -> None:
+    s3_resource = boto3.resource('s3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+
+    try:
+        print(f"Attempting to delete s3 file at path [{path_to_file}]")
+        s3_resource.Object(settings.S3_BUCKET, path_to_file).delete()
+        print(f"Successfully deleted s3 file [{path_to_file}]")
+    except Exception as ex:
+        raise Exception(f"Failed to delete s3 file [{path_to_file}] from bucket [{settings.S3_BUCKET}]. Details: {ex}")
+
+
+def fetch_files_in_dir(s3_dir: str, full_path: bool = True) -> list:
+    s3_client = boto3.client('s3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+
+    response = s3_client.list_objects_v2(
+        Bucket=settings.S3_BUCKET,
+        Prefix=f"{s3_dir}/")
+
+    file_list = []
+    for content in response.get('Contents', []):
+        if full_path:
+            file_list.append(content['Key'])
+        else:
+            file_path_bits = content['Key'].split()
+            file_list.append(file_path_bits[-1])
+    return file_list
+
+
+def move_photos(source_dir: str, dest_dir: str, file_names: list = []) -> None:
+    s3_resource = boto3.resource('s3',
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+
+    # if file_names not specified, fetch contents of dir from s3
+    if not file_names: 
+        file_names = fetch_files_in_dir(source_dir, full_path=False)
+
+    for fn in file_names:
+        source_file_path = f"{source_dir}/{fn}"
+        full_source_file_path = f"{settings.S3_BUCKET}/{source_file_path}" 
+        dest_file_path = f"{dest_dir}/{fn}"
+
+        try:
+            # copy file from source_dir to dest_dir
+            print(f"Attempting to copy s3 source_file_path={source_file_path} to dest_file_path={dest_file_path}")
+            s3_resource.Object(settings.S3_BUCKET, dest_file_path).copy_from(CopySource=full_source_file_path, ACL='public-read')
+            print(f"Successfully moved file_name [{fn}] from s3 dir [{source_dir}] to [{dest_dir}] on bucket [{settings.S3_BUCKET}]")
+
+            # delete file from source_dir
+            delete_file(source_file_path)
+
+        except Exception as ex:
+            raise Exception(f"Failed to move file_name [{fn}] from s3 dir [{source_dir}] to [{dest_dir}] on bucket [{settings.S3_BUCKET}]. Details: {ex}")
+
+    # delete_empty_dir(dir_to_delete)
+    
+
+def resize_and_upload(orig_img, thumb_type, img_dimensions, uuid):
+    print('****** in resize_and_upload')
+
+    print(f"@@@@@@ [{thumb_type} before resizing] orig_img.format: {str(orig_img.format)}")
+    print(f"@@@@@@ [{thumb_type} before resizing] orig_img.size: {str(orig_img.size)}")
+
+    # im = Image.open(orig_img)
+    orig_img.thumbnail(img_dimensions, Image.ANTIALIAS)
+    
+    print(f"****** [{thumb_type} after resizing] orig_img.format: {str(orig_img.format)}")
+    print(f"****** [{thumb_type} after resizing] orig_img.size: {str(orig_img.size)}")
+    
+    in_mem_file = BytesIO()
+    orig_img.save(in_mem_file, format=orig_img.format)
+
+    print(f"^^^^^^ [{thumb_type}] orig_img saved to in_mem_file")
+    print(f"^^^^^^ [{thumb_type}] file size / in_mem_file.tell(): {str(in_mem_file.tell())}")
+
+    in_mem_file.seek(0)
+
+    resized_img_file_name = f"{thumb_type}/{uuid}"
+
+    # Upload image to s3
+    client_s3 = boto3.client('s3') 
+
+    response = client_s3.put_object( 
+        ACL="public-read",
+        Bucket=settings.S3_BUCKET,
+        Body=in_mem_file,
+        ContentType='image/jpeg',
+        Key=resized_img_file_name,
+        Expires = datetime.now() + timedelta(minutes = 6),
+    )
+
+    resized_img_file_path = f"https://{settings.S3_BUCKET}.s3.amazonaws.com/{resized_img_file_name}"
+
+    print(f"====== [{thumb_type}] resized_img_file_path: {resized_img_file_path}")
+    print(f"====== [{thumb_type}] str(response): {str(response)}")
+
+    return resized_img_file_path
