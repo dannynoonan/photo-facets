5c116e1de053222576c61336c1db37173c660537
Andy Shirey <andyshirey@ilovelamp.local>
2022-09-25 03:02:08 -0700

    extract most db r/w out of views, not sure if this is the django way, but feels necessary for db integration tests

---
+++ lockdownsf/tests/integration_tests/test_dao.py
@@ -0,0 +1,10 @@
+import pytest
+
+from lockdownsf.models import Album
+
+
+@pytest.mark.django_db
+def test_create_album():
+    pass
+
+    
--- lockdownsf/tests/test_image_utils.py
+++
@@ -1,9 +0,0 @@
-from lockdownsf.services.image_utils import get_lat_lng
-
-def test_get_lat_lng():
-    exif_data = {
-        'GPSLatitude': (33.0, 59.0, 16.59), 
-        'GPSLongitude': (118.0, 25.0, 30.53), 
-    }
-    assert(get_lat_lng(exif_data) == 33.987870, -118.425180)
-    
