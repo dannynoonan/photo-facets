f3c6267d21f8a0a3a2ee88a248fee8650387f8ca
Andy Shirey <andyshirey@ilovelamp.local>
2022-09-25 22:33:48 -0700

    remove legacy google photos api references from javascript

--- lockdownsf/static/js/manage.js
+++ lockdownsf/static/js/manage.js
@@ -198,45 +198,6 @@ function makeAlbumNameEditable() {
 }
 
 
-
-
-// // https://developers.google.com/photos/library/reference/rest/v1/mediaItems/batchCreate?apix=true
-// var gphotos_client_id = "REMOVE";
-// var gphotos_api_key = "TODO";
-// // var gapi;
-
-// function authenticate() {
-//     return gapi.auth2.getAuthInstance()
-//         .signIn({scope: "https://www.googleapis.com/auth/photoslibrary https://www.googleapis.com/auth/photoslibrary.appendonly https://www.googleapis.com/auth/photoslibrary.sharing"})
-//         .then(function() { console.log("Sign-in successful"); },
-//               function(err) { console.error("Error signing in", err); });
-// }
-
-// function loadClient() {
-//     gapi.client.setApiKey(gphotos_api_key);
-//     return gapi.client.load("https://photoslibrary.googleapis.com/$discovery/rest?version=v1")
-//         .then(function() { console.log("GAPI client loaded for API"); },
-//               function(err) { console.error("Error loading GAPI client for API", err); });
-// }
-
-// // Make sure the client is loaded and sign-in is complete before calling this method.
-// function execute() {
-//     return gapi.client.photoslibrary.mediaItems.batchCreate({
-//         "resource": {}
-//     })
-//     .then(function(response) {
-//         // Handle the results here (response.result has the parsed body).
-//         console.log("Response", response);
-//     },
-//     function(err) { console.error("Execute error", err); });
-// }
-
-// gapi.load("client:auth2", function() {
-//     gapi.auth2.init({client_id: gphotos_client_id});
-// });
-
-
-
 var totalFileCount = 0;
 var successFileCount = 0;
 var failedFiles = [];
